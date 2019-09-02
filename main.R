7594eaf3d82cb2c556d565e3026e6ea17a96471b
dlguo <daleyguo@gmail.com>
2019-09-02 15:19:57 -0400

    update parameters

--- main.R
+++ main.R
@@ -8,13 +8,13 @@ set.seed(123)
 # Barabasi-Albert model
 ba.pop <- lapply(1:100, function(x) barabasi.game(100, m=5, directed = F))
 # Forest Fire model
-ff.pop <- lapply(1:100, function(x) forest.fire.game(100, .38, directed = F))
+ff.pop <- lapply(1:100, function(x) forest.fire.game(200, .38, directed = F))
 # Stochastic Block model
 pm <- matrix(0, nrow=3, ncol=3)
 pm[lower.tri(pm)] <- c(p12, p13, p23)
 pm <- pm+t(pm)
 diag(pm) <- c(p1, p2, p3)
-sbm.pop <- lapply(1:100, function(x) sample_sbm(100, pref.matrix = ma))
+sbm.pop <- lapply(1:100, function(x) sample_sbm(150, pref.matrix = pm, block.sizes = c(30, 70, 50)))
 
 ######   Section 4: Pairwise Evaluation   ######
 
