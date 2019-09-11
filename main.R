2ffb79d8a3d518244f4b653f947ed0c1b8838951
dlguo <daleyguo@gmail.com>
2019-09-11 16:44:25 -0400

    fix the SBM probs

--- main.R
+++ main.R
@@ -11,9 +11,9 @@ ba.pop <- lapply(1:100, function(x) barabasi.game(100, m=5, directed = F))
 ff.pop <- lapply(1:100, function(x) forest.fire.game(200, .38, directed = F))
 # Stochastic Block model
 pm <- matrix(0, nrow=3, ncol=3)
-pm[lower.tri(pm)] <- c(p12, p13, p23)
+pm[lower.tri(pm)] <- c(.07, .07, .07) # The cross-community probability for all communities are 0.07
 pm <- pm+t(pm)
-diag(pm) <- c(p1, p2, p3)
+diag(pm) <- c(.2, .15, .25) # The inner-community probability for three communities are 0.02, 0.15 and 0.25 respectively.
 sbm.pop <- lapply(1:100, function(x) sample_sbm(150, pref.matrix = pm, block.sizes = c(30, 70, 50)))
 
 ######   Section 4: Pairwise Evaluation   ######
